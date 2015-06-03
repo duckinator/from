09c1b43a87c2e6be5e49a8acf8e5d0dbb5475049
Marie Markwell <me@marie.so>
2015-06-03 07:38:57 -0400

    Clean up tests.

--- spec/from_spec.rb
+++ spec/from_spec.rb
@@ -7,30 +7,30 @@ RSpec.describe From do
   let(:collector) { Module.new }
 
   let(:fake_path)   { "some_library/test_file" }
-  let(:fake_path_allcaps) { "some_library/test_file/allcaps"}
+  let(:fake_path_a) { "some_library/test_file/a"}
   let(:fake_module) { Module.new }
-  let(:fake_module_allcaps) { Module.new }
+  let(:fake_module_a) { Module.new }
   let(:fake_module_b) { Module.new }
 
   subject { From.new(fake_path) }
 
   before do
     stub_const("SomeLibrary::TestFile", fake_module)
-    stub_const("SomeLibrary::TestFile::ALLCAPS", fake_module_allcaps)
+    stub_const("SomeLibrary::TestFile::A", fake_module_a)
     stub_const("SomeLibrary::TestFile::B", fake_module_b)
   end
 
   context '#module_from_path' do
     it 'resolves a module from a path name' do
-      expect(subject.send(:module_from_path, fake_path_allcaps)).to be(fake_module_allcaps)
+      expect(subject.send(:module_from_path, fake_path_a)).to be(fake_module_a)
     end
   end
 
   context '#import_to' do
     it 'imports the specified constants to the specified object' do
-      subject.send(:import_to, collector, :ALLCAPS, :B)
+      subject.send(:import_to, collector, :A, :B)
 
-      expect(collector::ALLCAPS).to be(fake_module_allcaps)
+      expect(collector::A).to be(fake_module_a)
       expect(collector::B).to be(fake_module_b)
     end
   end
